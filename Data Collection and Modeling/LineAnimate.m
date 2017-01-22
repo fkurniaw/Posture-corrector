1170aa12073cd000183dad7b51fd8dc693ab3584
Felix Kurniawan <fkurniaw@uwaterloo.ca>
2017-01-21 22:20:02 -0500

    Transpose Variable and Change Axes Limit

--- Data Collection and Modeling/LineAnimate.m
+++ Data Collection and Modeling/LineAnimate.m
@@ -5,8 +5,8 @@ axis(gca,'equal');
 axis([-1 25 -1 25]);
 for t=1:3081
     hold on,
-    theta1 = data(t);
-    theta2 = data2(t);
+    theta1 = lowerMPU(t);
+    theta2 = (upperMPU(t))';
     P2 = length*[cos(theta1), sin(theta1)];
     P3 = length*[cos(theta2), sin(theta2)]+ P2;
     crank1 = line([vertex(1), P2(1)], [vertex(2), P2(2)]);
